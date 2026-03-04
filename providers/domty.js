c349373875ebf906be7d6f0af5bbd8ac98ebe34f
D3adlyRocket <31734254+D3adlyRocket@users.noreply.github.com>
2026-03-04 10:03:34 +0000

    Update domty.js

--- providers/domty.js
+++ providers/domty.js
@@ -1,58 +1,21 @@
 var BASE = "https://cima4u.tv";
 
 var DEFAULT_HEADERS = {
-  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+  "User-Agent": "Mozilla/5.0",
   "Accept-Language": "en-US,en;q=0.9",
   "Referer": BASE
 };
 
-function http(url, headers) {
-  return fetch(url, {
-    headers: Object.assign({}, DEFAULT_HEADERS, headers || {})
-  }).then(r => r.text());
+function http(url) {
+  return fetch(url, { headers: DEFAULT_HEADERS }).then(r => r.text());
 }
 
-function extractStreams(html, referer) {
-  var results = [];
-  var re = /(https?:\/\/[^"' ]+\.(m3u8|mp4)[^"' ]*)/g;
-  var m;
-
-  while ((m = re.exec(html)) !== null) {
-    results.push({
-      name: "Cima4U",
-      url: m[1],
-      quality: "HD",
-      headers: { Referer: referer }
-    });
-  }
-
-  return results;
-}
-
-function findMoviePage(html) {
-  var m = html.match(/<a href="(https?:\/\/[^"]+)"[^>]*class="[^"]*movie/i);
-  if (m) return m[1];
-
-  m = html.match(/<a href="(https?:\/\/[^"]+)"/);
-  return m ? m[1] : null;
-}
-
-function findWatchPage(html) {
-  var m = html.match(/href="(https?:\/\/[^"]+watch[^"]+)"/i);
-  if (m) return m[1];
-
-  m = html.match(/data-url="(https?:\/\/[^"]+)"/i);
-  return m ? m[1] : null;
-}
-
-function getTitle(tmdbId, mediaType) {
-  var type = mediaType === "movie" ? "movie" : "tv";
+function getTitle(id, type) {
 
   var url =
     "https://api.themoviedb.org/3/" +
-    type +
-    "/" +
-    tmdbId +
+    (type === "movie" ? "movie/" : "tv/") +
+    id +
     "?api_key=44c4c1f0a1d7b6f0a4c3a1e8e6e1c111";
 
   return fetch(url)
@@ -60,35 +23,52 @@ function getTitle(tmdbId, mediaType) {
     .then(j => j.title || j.name);
 }
 
+function extract(html, referer) {
+
+  var out = [];
+  var re = /(https?:\/\/[^"' ]+\.(m3u8|mp4)[^"' ]*)/g;
+  var m;
+
+  while ((m = re.exec(html))) {
+    out.push({
+      name: "CIMA4U",
+      url: m[1],
+      quality: "HD",
+      headers: { Referer: referer }
+    });
+  }
+
+  return out;
+}
+
 function getStreams(tmdbId, mediaType, season, episode) {
-  return getTitle(tmdbId, mediaType).then(title => {
 
-    var searchUrl = BASE + "/?s=" + encodeURIComponent(title);
+  return getTitle(tmdbId, mediaType).then(function(title) {
 
-    return http(searchUrl).then(searchHtml => {
+    var search = BASE + "/?s=" + encodeURIComponent(title);
 
-      var moviePage = findMoviePage(searchHtml);
-      if (!moviePage) return [];
+    return http(search).then(function(html) {
 
-      return http(moviePage).then(movieHtml => {
+      var match = html.match(/<a href="(https?:\/\/[^"]+)"/i);
+      if (!match) return [];
 
-        var watchPage = findWatchPage(movieHtml) || moviePage;
+      var moviePage = match[1];
 
-        return http(watchPage).then(watchHtml => {
+      return http(moviePage).then(function(page) {
 
-          var streams = extractStreams(watchHtml, watchPage);
+        var streams = extract(page, moviePage);
 
-          // also scan iframes
-          var iframeMatch = watchHtml.match(/<iframe[^>]+src="([^"]+)"/i);
+        var iframe = page.match(/<iframe[^>]+src="([^"]+)"/i);
 
-          if (!streams.length && iframeMatch) {
-            return http(iframeMatch[1]).then(iframeHtml =>
-              extractStreams(iframeHtml, iframeMatch[1])
-            );
-          }
+        if (!streams.length && iframe) {
 
-          return streams;
-        });
+          return http(iframe[1]).then(function(iframeHtml) {
+            return extract(iframeHtml, iframe[1]);
+          });
+
+        }
+
+        return streams;
       });
     });
   });
