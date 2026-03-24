46ec039386524bdef152603758411e4e878b63d6
D3adlyRocket <31734254+D3adlyRocket@users.noreply.github.com>
2026-03-24 16:11:40 +0000

    Update dahmermovies.js

--- providers/dahmermovies.js
+++ providers/dahmermovies.js
@@ -4,9 +4,9 @@
 console.log('[DahmerMovies] Initializing Dahmer Movies scraper');
 
 // Constants
-const TMDB_API_KEY = "439c478a771f35c05022f9feabcca01c";
+const TMDB_API_KEY = "8ab2887a5c9ac2a523356811180f5166";
 const DAHMER_MOVIES_API = 'https://a.111477.xyz';
-const TIMEOUT = 240000; // 240 seconds
+const TIMEOUT = 60000; // 60 seconds
 
 // Quality mapping
 const Qualities = {
